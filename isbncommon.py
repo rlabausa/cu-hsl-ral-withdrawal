2391f2bf4e979f9a977dfda3b82b41bdeb2ff9a6
rlabausa <rubylabausa@gmail.com>
2022-07-11 22:48:57 -0500

    Format

--- isbncommon.py
+++ isbncommon.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
-def find_desired_val_from_search_val(search_val, ws, ws_search_val_col_index, ws_desired_val_col_index):
+
+def find_desired_val_from_search_val(
+    search_val, ws, ws_search_val_col_index, ws_desired_val_col_index
+):
     """Returns the desired value from a `Worksheet` given a unique, non-empty search value and location details to match.
 
     Keyword arguments:
@@ -10,23 +13,23 @@ def find_desired_val_from_search_val(search_val, ws, ws_search_val_col_index, ws
     ws_desired_val_col_index -- one-based index of the column in the `Worksheet` containing desired value
     """
     # Note: `row` is a tuple of cells, i.e., `row` has a zero-based index
-    #        each `cell` in the row, however, has a one-based index 
-    for row in ws.iter_rows(min_row = 2):
+    #        each `cell` in the row, however, has a one-based index
+    for row in ws.iter_rows(min_row=2):
         val = row[ws_search_val_col_index - 1].value
-        
+
         # stop searching if search val is empty
-        if(search_val is None or search_val == ''):
+        if search_val is None or search_val == "":
             return None
-        
-        if(search_val is not None and val is not None):
+
+        if search_val is not None and val is not None:
             search_val_type = type(val)
             val_type = type(val)
 
             # cast to string
             search_val = str(search_val)
             val = str(val)
-            
-            if(search_val_type == str and val_type == str):
+
+            if search_val_type == str and val_type == str:
                 # remove whitespace for string values
                 search_val = search_val.strip()
                 val = val.strip()
@@ -34,37 +37,36 @@ def find_desired_val_from_search_val(search_val, ws, ws_search_val_col_index, ws
                 # ensure case-insensitivity
                 search_val = search_val.upper()
                 val = val.upper()
-            
+
             # check if value is what we're looking for
-            if(search_val == val):
+            if search_val == val:
                 desired_val = row[ws_desired_val_col_index - 1].value
                 return desired_val
     return None
 
+
 def find_row_from_title(title_to_find, ws, ws_title_col_index):
     for row in ws.rows:
         title = row[ws_title_col_index - 1].value
-        
-        if(title == title_to_find):
+
+        if title == title_to_find:
             return row
     return None
 
-def find_row_from_search_val(search_val, ws, search_val_col_index):
-    for row in ws.iter_rows(min_row = 2):
-        val = row[search_val_col_index - 1].value
-        
-        # stop searching if search val is empty
-        if(search_val is None or search_val == ''):
-            return None
-        
-        if(search_val is not None and val is not None):
-            search_val_type = type(val)
-            val_type = type(val)
 
+def find_row_from_search_val(search_val, ws, search_val_col_index):
+    for row in ws.iter_rows(min_row=2):
+        val = row[search_val_col_index - 1].value
+
+        # stop searching if search val is empty
+        if search_val is None or search_val == "":
+            return None
+
+        if search_val is not None and val is not None:
             # cast to string
             search_val = str(search_val)
             val = str(val)
-            
+
             # remove whitespace for string values
             search_val = search_val.strip()
             val = val.strip()
@@ -72,14 +74,13 @@ def find_row_from_search_val(search_val, ws, search_val_col_index):
             # ensure case-insensitivity
             search_val = search_val.upper()
             val = val.upper()
-            
+
             # check if value is what we're looking for
-            if(search_val == val):
+            if search_val == val:
                 return row
     return None
 
-    
-   
+
 def get_xlsx_files(dir_path):
     """Returns the list of .xlsx files as `Path` objects in a directory
 
@@ -87,24 +88,26 @@ def get_xlsx_files(dir_path):
     dir_path -- the directory to find files
     """
     p = Path(dir_path)
-    files = list(p.glob('*.xlsx'))
+    files = list(p.glob("*.xlsx"))
     return files
+
 
 def print_worksheet_names(wb):
     """Print the names of all Worksheets in a given Workbook.
 
-    Keyword arguments: 
+    Keyword arguments:
     wb -- the Workbook containing desired Worksheet names to print
     """
     for ws in wb.worksheets:
         print(ws.title)
 
+
 def print_worksheet_header_columns(ws):
     """Prints the value and coordinates of all header columns in a Worksheet.
 
-    Keyword arguments: 
+    Keyword arguments:
     ws -- the Worksheet containing desired column headers to print
     """
-    for row in ws.iter_rows(max_row = 1):
+    for row in ws.iter_rows(max_row=1):
         for cell in row:
             print(cell.coordinate, cell.value)
